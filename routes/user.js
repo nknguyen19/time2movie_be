3d1ec41bfeb11c1903355df087ea1c0fb7e52b53
peternguyen39 <nqktuyen19@apcs.fitus.edu.vn>
2022-05-13 21:38:55 +0700

    Finish Google Login

--- routes/user.js
+++ routes/user.js
@@ -9,5 +9,6 @@ router.post('/login-facebook', user.login_facebook);
 router.get('/get-user/:id', user.get_user);
 router.post('/signin', user.signin);
 router.get('/get-current-user', user.get_current_user);
+router.post('/login-google', user.login_google);
 
 module.exports = router
